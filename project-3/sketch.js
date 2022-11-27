697fe9f7d9219777bd55cc92ea987df268283baf
Catchen2 <117329374+Catchen2@users.noreply.github.com>
2022-11-27 11:35:27 -0800

    Update sketch.js

--- project-3/sketch.js
+++ project-3/sketch.js
@@ -15,12 +15,12 @@ function draw() {
   for (let a = 60; a < windowHeight; a = a + 60) {
     strokeWeight(0.6);
     stroke(255);
-    line(0, a, canvasWidth, a);
+    line(0, a, windowWidth, a);
   }
 
   for (let a = 60; a < windowWidth; a = a + 60) {
     strokeWeight(0.6);
     stroke(255);
-    line(a, 0, a, canvasHeight);
+    line(a, 0, a, windowHeight);
   }
 }
