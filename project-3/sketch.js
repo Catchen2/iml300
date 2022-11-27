cec504fc11d7c75bf8bee6194bb3cc3a28c77992
Catchen2 <117329374+Catchen2@users.noreply.github.com>
2022-11-27 13:05:26 -0800

    Update sketch.js

--- project-3/sketch.js
+++ project-3/sketch.js
@@ -1,24 +1,24 @@
 
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(2000, 2000);
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
-  canvas.resize(windowWidth, windowHeight);
+
 }
 
 function draw() {
   background(0);
 
-  for (let a = 60; a < windowHeight; a = a + 60) {
+  for (let a = 60; a < 2000; a = a + 60) {
     strokeWeight(0.6);
     stroke(255);
-    line(0, a, windowWidth, a);
+    line(0, a, 2000, a);
   }
 
-  for (let a = 60; a < windowWidth; a = a + 60) {
+  for (let a = 60; a < 2000; a = a + 60) {
     strokeWeight(0.6);
     stroke(255);
-    line(a, 0, a, windowHeight);
+    line(a, 0, a, 2000);
   }
 }
