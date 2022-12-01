7cdbc2dc5d24b4545f23c9aa174213cd134d4d8b
Catchen2 <117329374+Catchen2@users.noreply.github.com>
2022-11-30 16:50:59 -0800

    Update sketch3.js

--- project-3/sketch3.js
+++ project-3/sketch3.js
@@ -6,31 +6,38 @@ let nextChange3 = timer + timer + timer + timer;
 let nextChange4 = timer + timer + timer + timer + timer;
 let nextChange5 = timer + timer + timer + timer + timer + timer;
 let nextChange6 = timer + timer + timer + timer + timer + timer + timer;
+let nextChange7 = timer + timer + timer + timer + timer + timer + timer + timer;
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
-  canvas.position(0, 0);
-  canvas.style("z-index", "-1");
+  var cnv = createCanvas(800, 800);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2 - 50;
+  cnv.position(x, y);
+  cnv.style("z-index", "-1");
+  
 }
 
 function draw() {
+
   fill(255);
   stroke(255);
   textSize(12);
   strokeWeight(1);
 
-  let x = 250;
-  let y = 500;
+  let x = 20;
+  let y = 760;
 
   if (millis() > nextChange) {
     strokeWeight(1);
     circle(x, y, 10);
     text("2011", x - 12, y - 20, 40, 40);
+    text("$11", x - 12, y +10, 40, 40);
     nextChange = millis() + timer;
   }
   if (millis() > nextChange1) {
     strokeWeight(1);
     text("2012", x + 106, y - 45, 40, 40);
+    text("$13", x + 106, y - 15, 40, 40);
     strokeWeight(2);
     circle(x + 120, y - 25, 10);
     line(x, y, x + 120, y - 25);
@@ -39,6 +46,7 @@ function draw() {
   if (millis() > nextChange2) {
     strokeWeight(1);
     text("2013", x + 226, y - 48, 40, 40);
+    text("$13", x + 226, y - 18, 40, 40);
     strokeWeight(2);
     circle(x + 240, y - 28, 10);
     line(x + 120, y - 25, x + 240, y - 28);
@@ -48,6 +56,7 @@ function draw() {
   if (millis() > nextChange3) {
     strokeWeight(1);
     text("2014", x + 344, y - 50, 40, 40);
+    text("$13.5", x + 344, y - 20, 40, 40);
     strokeWeight(2);
     circle(x + 360, y - 30, 10);
     line(x + 240, y - 28, x + 360, y - 30);
@@ -57,6 +66,7 @@ function draw() {
   if (millis() > nextChange4) {
     strokeWeight(1);
     text("2015", x + 464, y - 420, 40, 40);
+    text("$750", x + 464, y - 390, 40, 40);
     strokeWeight(2);
     circle(x + 480, y - 400, 10);
     line(x + 360, y - 30, x + 480, y - 400);
@@ -66,6 +76,7 @@ function draw() {
   if (millis() > nextChange5) {
     strokeWeight(1);
     text("2016", x + 586, y - 420, 40, 40);
+    text("$750", x + 586, y - 390, 40, 40);
     strokeWeight(2);
     ellipse(x + 600, y - 400, 10);
     line(x + 480, y - 400, x + 600, y - 400);
@@ -75,6 +86,7 @@ function draw() {
   if (millis() > nextChange6) {
     strokeWeight(1);
     text("2017", x + 706, y - 420, 40, 40);
+    text("$750", x + 706, y - 390, 40, 40);
     strokeWeight(2);
     ellipse(x + 720, y - 400, 10);
     line(x + 600, y - 400, x + 720, y - 400);
