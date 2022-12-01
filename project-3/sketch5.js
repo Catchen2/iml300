030fe5ab11e901ad6233eec73abd513d38bacb14
Catchen2 <117329374+Catchen2@users.noreply.github.com>
2022-11-30 17:22:04 -0800

    Update sketch5.js

--- project-3/sketch5.js
+++ project-3/sketch5.js
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
 let imgs = [];
 
 function preload() {
   for (var i = 0; i < 2; i++) {
-    imgs[i] = loadImage("asset/pill.png");
+    imgs[i] = loadImage("https://cdn.glitch.global/3c6c63b1-b30e-4912-9dcc-f9f5cb30d592/pill.png?v=1669847759376");
   }
 }
 
@@ -23,29 +22,3 @@ function draw() {
     image(imgs[i], random(width), random(height), 20, 45);
   }
 }
-=======
-let imgs = [];
-
-function preload() {
-  for (var i = 0; i < 2; i++) {
-    imgs[i] = loadImage("asset/pill.png");
-  }
-}
-
-function setup() {
-  var cnv = createCanvas(windowWidth, windowHeight);
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) /2 ;
-  cnv.position(x, y);
-  cnv.style("z-index", "-1");
-  
-  imageMode(CENTER);
-  frameRate(4);
-}
-
-function draw() {
-  for (var i = 1; i < 2; i++) {
-    image(imgs[i], random(width), random(height), 20, 45);
-  }
-}
->>>>>>> 20ce7c8dc0c2c4af125f03b69012376dfe7f671b
